8d60aa71a3aa16544f534aea3068584b6db68ef1
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-07-31 14:30:45 -0300

    commit inicial

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { WhiteHeart, RedHeart } from './styled';
+
+function Card() {
+  return (
+    <div>
+      <p>02 Store</p>
+      <WhiteHeart />
+      <RedHeart />
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Card/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Heart } from '@styled-icons/evil/Heart'
+import {Heart as FilledHeart} from '@styled-icons/evaicons-solid/Heart'
+
+export const WhiteHeart = styled(Heart)`
+  color: #DC143C;
+  width: 30px;
+  font-weight: 200;
+`;
+
+export const RedHeart = styled(FilledHeart)`
+  color: #DC143C;
+  width: 30px;
+  font-weight: 200;
+`;
+
